eb3736ba277c5411e316fcc754d9d2891c83b461
DmitriyKukshinov <kukshinofff@mail.ru.com>
2025-02-22 01:27:58 +0500

    Добавил в оглавление вывод названий глав и статей из БД. Добавил в оглаление многоуровневый список.

--- ClusterTextbook/articles/views.py
+++ ClusterTextbook/articles/views.py
@@ -1,14 +1,18 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Chapter, Article
 
 
 def index(request):
     # Создаётся шаблон на основе странички
     template = loader.get_template('index.html')
 
+    # Получаю список всех глав, сортирую по номеру и заранее загружаю связанные с ними статьи через prefetch_related
+    chapter_list = Chapter.objects.order_by("chapter_order").prefetch_related("article_set")
     # Это данные, которые нужно подставить в шаблон.
-    # В этой конкретной ситуации данные туда подставлять не нужно, поэтому словарь остаётся пустым.
-    context = {}
+    context = {
+        "chapter_list": chapter_list
+    }
 
     # Шаблон превращается в “отрендеренную страницу” после того, как туда подставлены данные.
     rendered_page = template.render(context, request)
@@ -17,6 +21,8 @@ def index(request):
 
 def article_page(request, article_name:str):
     template = loader.get_template(f'{article_name}.html')
+
+    # В данном случае никаких данных вставлять не нужно и контекста нет
     context = {}
     rendered_page = template.render(context, request)
     return HttpResponse(rendered_page)
