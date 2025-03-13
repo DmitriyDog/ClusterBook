ce298301860ad652f919efe5e7f9b94785a8e870
DmitriyKukshinov <kukshinofff@mail.ru.com>
2025-03-13 23:39:31 +0500

    Добавил функцию поиска по статьям на навигационную панель. Показывает 3 наиболее подходящих результата

--- ClusterTextbook/articles/urls.py
+++ ClusterTextbook/articles/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.index),
     path("articles/<int:article_id>/", views.article_page, name='article_page'),
     path('articles/<int:article_id>/mark-as-read/', views.mark_article_read, name='mark_article_read'),
+    path("search", views.search_article, name="search_article")
 ]
--- ClusterTextbook/articles/views.py
+++ ClusterTextbook/articles/views.py
@@ -4,6 +4,8 @@ from .models import Chapter, Article, ArticleBlock, ImageBlock
 from .forms import ReadArticleForm
 from django.core.cache import cache
 from django.shortcuts import redirect
+from .side_algorithms.kmp import KMPSearch
+from django.http import JsonResponse
 
 
 def index(request):
@@ -37,6 +39,7 @@ def article_page(request, article_id:int):
     previous_article = (Article.objects.filter(article_order__lt=opened_article.article_order)
                         .order_by("-article_order").first())
     cache_list = [article.pk for article in Article.objects.all() if cache.get(f"article_{article.pk}_read")]
+
     context = {
         "chapter_list": chapter_list,
         "article_content": article_content,
@@ -66,10 +69,52 @@ def mark_article_read(request, article_id: int):
         if current_state is None or not current_state:
             # Отмечаем прочитанную статью в кэше
             cache.set(f"article_{article_id}_read", True, timeout=None)
-            print(cache.get(f"article_{article_id}_read"))  # Проверка
         else:
             # Если статья уже помечена как прочитанная, снимаем отметку
             cache.delete(f"article_{article_id}_read")
-            print(cache.get(f"article_{article_id}_read"))  # Проверка
 
     return redirect(f"/articles/{article_id}/")
+
+
+'''
+Релевантность результата зависит от того, где встречается искомая строка:
+Название статьи = 1000 очков
+Подзаголовок статьи = 200 очков
+Текст статьи = 1 очко
+'''
+def search_article(request):
+    if request.method == "POST":
+        search_query = request.POST.get("q", "")
+        if search_query:
+            # Поиск
+            article_list = Article.objects.prefetch_related("articleblock_set").all()
+
+            # В качестве результата выдается 3 лучших (релевантных) статьи, которые набрали наибольшее количество очков
+            score_count = []
+            for article in article_list:
+                score = 0
+
+                # Использую алгоритм Кнута — Морриса — Пратта для поиска подстроки
+                score += len(KMPSearch(search_query, article.article_name)) * 1000
+                header_list = article.articleblock_set.filter(type_of_text="header")
+                text_list = article.articleblock_set.filter(type_of_text="text")
+                for header in header_list:
+                    score += len(KMPSearch(search_query, header.text_block)) * 200
+                for paragraph in text_list:
+                    score += len(KMPSearch(search_query, paragraph.text_block))
+                if score_count and len(score_count) == 3 and score > score_count[-1][-1]:
+                        score_count[-1] = (article.pk, article.article_name, score)
+                elif score != 0:
+                    score_count.append((article.pk, article.article_name, score))
+                score_count.sort(key=lambda x: x[-1], reverse=True)
+            if score_count:
+                context = {"search_results": [{
+                    "article_name": elem[1],
+                    "pk": elem[0]
+                } for elem in score_count]}
+            else:
+                context = {}
+        else:
+            context = {}
+        return JsonResponse(context)
+    return HttpResponse("Произошла ошибка на стороне сайта", status=404)
