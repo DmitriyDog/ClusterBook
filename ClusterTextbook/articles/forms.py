14407bd7d34ac5c67c87ac1dc62ee6bafeddf2dc
Эдуард <BezvodEd@yandex.ru>
2025-03-15 15:22:20 +0500

    создан админ и все прочее

--- ClusterTextbook/articles/forms.py
+++ ClusterTextbook/articles/forms.py
@@ -3,8 +3,7 @@ from django import forms
 
 class ReadArticleForm(forms.Form):
     article_state = forms.BooleanField(label="Статья прочитана:",
-                                        initial=False,
-                                        required=False,
-                                        widget=forms.CheckboxInput(attrs={
-                                            "class": "big-checkbox"}))
+                                       initial=False,
+                                       required=False,
+                                       widget=forms.CheckboxInput(attrs={"class": "big-checkbox"}))
     required_css_class = "boolean-style"
